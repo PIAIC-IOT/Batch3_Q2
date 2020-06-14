b375c4991a1c08f90268d295e03952f0a7b9da25
imran82ali <code.imranali@gmail.com>
2020-06-14 11:34:37 +0500

    June 14, 2020 and STM configutaion on Windows

---
+++ 2020_06_14/leftshift/target/rls/debug/deps/leftshift-3583c7abdf2cd0b3.d
@@ -0,0 +1,5 @@
+/home/imran82ali/batch3/q2/2020_06_14/leftshift/target/rls/debug/deps/leftshift-3583c7abdf2cd0b3.rmeta: src/main.rs
+
+/home/imran82ali/batch3/q2/2020_06_14/leftshift/target/rls/debug/deps/leftshift-3583c7abdf2cd0b3.d: src/main.rs
+
+src/main.rs:
---
+++ 2020_06_14/leftshift/target/rls/debug/deps/leftshift-4158c57d6d68668e.d
@@ -0,0 +1,5 @@
+/home/imran82ali/batch3/q2/2020_06_14/leftshift/target/rls/debug/deps/leftshift-4158c57d6d68668e.rmeta: src/main.rs
+
+/home/imran82ali/batch3/q2/2020_06_14/leftshift/target/rls/debug/deps/leftshift-4158c57d6d68668e.d: src/main.rs
+
+src/main.rs:
